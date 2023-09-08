cc74d6dd4a4d0f94a9aeccffcfcc62f1f2dd73c7
Ulrich <uhgall@gmail.com>
2023-09-08 18:18:05 -0500

    strawmen

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # scicliffs
 The Quick Dive into Deep Science
+
+## Pitch
+
+Join the AI Revolution! 🚀 We're launching a crowdsourced, ever-evolving distributed network of online science magazines. By using our browser extension, you're not just a reader - you contribute! Leveraging AI and large language models, we auto-archive, extract, and transform science papers into accessible articles. As our user base grows, so does our content. Dive in, and let's make* science universally understandable together!
+
+## Architecture
+
+* a web service that simply consumes a PDF file?
+* for purposes of hackathon, we can hardcode destination of where the generated article is posted
+
+## TODO - Essential
+
+* Converting PDF to text. Ulrich has a minimalist solution, but it's not great
+* Generate the actual content. See gen/generator.rb as a strawman. Lots of ways to improve it.
+
+## TODO - Stretch Goals
+
+* Generate images and add them to the article. Ulrich already added code to generate the prompts to gen/generate.rb
+
+* More elegant sources of articles
+- paste URL?
+- mod a browser plugin that is design to upload papers elsewhere? 
+- Just use the arxiv feed? 
+
+* Automatically submit them to substack? But, no API? Rob? 
+
+* Allow users to describe their style, audience, etc?
---
+++ gen/generator.rb
@@ -0,0 +1,51 @@
+require 'pdf-reader'
+require_relative '../../ruby-llm/lib/globals'
+
+pdf_name = "2305.14992.pdf"
+pdf_file_path = File.join('input',pdf_name)
+reader = PDF::Reader.new(pdf_file_path)
+
+full_text_from_paper = ""
+reader.pages.each do |page|
+    full_text_from_paper += page.text
+end
+
+glim = GlimContext.new
+
+req_article = glim.request_from_template('article', full_text_from_paper:)
+article_completion = req_article.response.completion
+_, files = extract_and_save_files(article_completion)
+article = files["article.md"]
+
+# do these in parallel
+req_headline = glim.request_from_template('headline', article:)
+req_teaser = glim.request_from_template('teaser', article:)
+req_images = glim.request_from_template('images', article:)
+
+headline = req_headline.response.completion
+teaser = req_teaser.response.completion
+
+_, files = extract_and_save_files(req_images.response.completion)
+image_prompts = files["prompts.txt"].split("\n")
+
+output_text = "# #{headline} \n\n #{teaser}\n\n #{article}" 
+
+output_text += "\n\n-----\nImage prompts (TODO):\n" + image_prompts.join("\n")
+
+outfilename = pdf_name.gsub('.pdf', '.md')
+f = File.open(File.join('output',outfilename), 'w')
+f.write(output_text)
+f.close
+
+
+
+# Or we could do something more structured, like:
+#
+# Headline
+# Why should I care?
+# What is the main contribution?
+# Summary
+# Introduction
+# How did they do it
+# What were their conclusions
+
