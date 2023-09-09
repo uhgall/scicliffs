dcafe623f0077fe8a58f5a8df71b41ae1e069f1f
Robb Walters <robb@scienft.com>
2023-09-09 12:52:13 -0700

    100 redirects

--- gen/generator.rb
+++ gen/generator.rb
@@ -1,7 +1,6 @@
 require 'pdf-reader'
 require 'glim_ai'
-# require_relative 'utils'
-
+require_relative 'utils'
 
 # for now, this just returns one string with everything in it 
 # later we will want to be more structured
@@ -79,69 +78,12 @@ def generate_article(pdf_file_path)
     # What were their conclusions
 end
 
-<<<<<<< HEAD
 
 # pdf_name = "2305.14992.pdf"
 # #pdf_name = "0601108.pdf" # that one weirdly doesn't work, not sure why, article is empty but completion looks right
-
 # pdf_file_path = File.join('input',pdf_name)
-=======
-glim = GlimContext.new
 
-# do these in parallel
-req_article = glim.request_from_template('article', full_text_from_paper:)
-req_article.llm_name = forced_default_llm if forced_default_llm
-req_errata = glim.request_from_template('errata', full_text_from_paper:)
-req_errata.llm_name = forced_default_llm if forced_default_llm
-
-article_completion = req_article.response.completion
-_, files = extract_and_save_files(article_completion)
-article = files["article.md"]
-
-raise "No article found in completion:\n#{article_completion}" if article.nil? || article.empty?
-
-# do these in parallel
-req_headline = glim.request_from_template('headline', article:)
-req_headline.llm_name = forced_default_llm if forced_default_llm
-
-req_teaser = glim.request_from_template('teaser', article:)
-req_teaser.llm_name = forced_default_llm if forced_default_llm
-
-req_images = glim.request_from_template('images', article:)
-req_images.llm_name = forced_default_llm if forced_default_llm
-
-headline = req_headline.response.completion
-teaser = req_teaser.response.completion
-
-_, files = extract_and_save_files(req_images.response.completion)
-image_prompts = files["prompts.txt"].split("\n")
-
-output_text = "# #{headline} \n\n #{teaser}\n\n #{article}" 
-
-output_text += "\n\n-----\nImage prompts (TODO):\n" + image_prompts.join("\n")
-
-errata = req_errata.response.completion
-output_text += "\n\n-----\nErrata:\n\n #{errata}\n"
-
-
-outfilename = pdf_name.gsub('.pdf', '.md')
-f = File.open(File.join('output',outfilename), 'w')
-f.write(output_text)
-f.close
-
-cost = [req_article, req_headline, req_teaser, req_images].map { |x| x.response.total_cost }.sum
-puts "Total Cost: $ #{cost}"
-
-
-# Or we could do something more structured, like:
-#
-# Headline
-# Why should I care?
-# What is the main contribution?
-# Summary
-# Introduction
-# How did they do it
-# What were their conclusions
->>>>>>> 137748718e778af2dc1f93b0f79f15fabb873acb
+pdf_file_path = fetch_arxiv_paper('2307.12008')
+puts pdf_file_path
 
 # puts generate_article(pdf_file_path)
--- gen/utils.rb
+++ gen/utils.rb
@@ -1,6 +1,13 @@
 require 'arx'
 require 'net/http'
 require 'uri'
+require 'arx'
+require 'net/http'
+require 'uri'
+
+require 'arx'
+require 'net/http'
+require 'uri'
 
 def fetch_arxiv_paper(id)
   paper = Arx(id)
@@ -10,23 +17,39 @@ def fetch_arxiv_paper(id)
   # Define the path to save the file
   file_path = File.join('..', 'input', "#{id}.pdf")
 
-
   # Use Net::HTTP to download the file
   Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
-    request = Net::HTTP::Get.new(uri)
+    max_redirects = 100
+    redirect_count = 0
 
-    http.request(request) do |response|
-      # Open a file for writing
-      File.open(file_path, 'wb') do |file|
-        # Stream the file to disk
-        response.read_body do |chunk|
-          file.write(chunk)
-        end
+    response = nil
+    while redirect_count < max_redirects
+      request = Net::HTTP::Get.new(uri)
+      # Add a User-Agent header
+      request['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537'
+      response = http.request(request)
+
+      puts redirect_count, response
+
+      break unless response.is_a?(Net::HTTPRedirection)
+
+      uri = URI.parse(response['location'])
+      redirect_count += 1
+    end
+
+    if response.is_a?(Net::HTTPRedirection)
+      raise "Too many redirects" 
+    end
+    
+    # Open a file for writing
+    File.open(file_path, 'wb') do |file|
+      # Stream the file to disk
+      response.read_body do |chunk|
+        file.write(chunk)
       end
     end
   end
 
+  # Return the file_path
+  file_path
 end
-
-
-
