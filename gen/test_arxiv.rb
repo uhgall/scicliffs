9b9eef6a2c170dbf0019b79eb66c5c4e638656ea
Ulrich <uhgall@gmail.com>
2023-09-09 15:43:17 -0500

    caching .md files

--- gen/test_arxiv.rb
+++ gen/test_arxiv.rb
@@ -1,7 +1,11 @@
 require_relative 'utils.rb'
 require_relative 'generator.rb'
 
-pdf_file_path = fetch_arxiv_paper('2307.12008')
-puts pdf_file_path
+id = '2307.12008'
 
-puts generate_article(pdf_file_path)
+puts get_article_for_arxiv_id(id)
+
+# pdf_file_path = fetch_arxiv_paper('2307.12008')
+# puts pdf_file_path
+
+# puts generate_article(pdf_file_path)
