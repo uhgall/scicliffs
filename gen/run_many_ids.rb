d4af148dc4f66138a2da6f904707bd798e1a9fd2
Ulrich <uhgall@gmail.com>
2023-09-09 16:24:38 -0500

    sync

---
+++ gen/run_many_ids.rb
@@ -0,0 +1,12 @@
+require_relative 'utils.rb'
+require_relative 'generator.rb'
+
+ids = ['2307.12008', '2307.12008', '2302.13971v1' , '2303.08774v3']
+
+for id in ids
+    puts get_article_for_arxiv_id(id)
+end
+# pdf_file_path = fetch_arxiv_paper('2307.12008')
+# puts pdf_file_path
+
+# puts generate_article(pdf_file_path)
