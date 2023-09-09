328aa8191a500a942453c09ad41c9039809072d7
Robb Walters <robb@scienft.com>
2023-09-09 14:07:22 -0700

    add link to original work

--- app.rb
+++ app.rb
@@ -4,12 +4,27 @@ require_relative 'gen/generator'
 
 require 'redcarpet'
 
-def markdown_to_html(markdown_content)
-  renderer = Redcarpet::Render::HTML.new
-  markdown = Redcarpet::Markdown.new(renderer)
-  html_content = markdown.render(markdown_content)
-  html_content
-end
+def markdown_to_html(markdown_content, original_work_uri)
+    renderer = Redcarpet::Render::HTML.new
+    markdown = Redcarpet::Markdown.new(renderer)
+    html_content = markdown.render(markdown_content)
+  
+    # Append a link to the original work URI before the closing body tag
+    link_html = "<p><a href='#{original_work_uri}'>Original Work</a></p>"
+  
+    # Check if the closing body tag exists
+    if html_content.include?("</body>")
+      # Insert the link before the closing body tag
+      insert_position = html_content.index("</body>")
+      html_content.insert(insert_position, link_html)
+    else
+      # If there's no closing body tag, just append the link
+      html_content += link_html
+    end
+    
+    html_content
+  end
+  
 
 
 get '/:id' do
@@ -22,6 +37,6 @@ get '/:id' do
   markdown_content = generate_article(pdf_file_path)
   puts markdown_content
 
-  html_content = markdown_to_html(markdown_content)
+  html_content = markdown_to_html(markdown_content, "https://arxiv.org/abs/" + arxiv_id)
   html_content
 end
